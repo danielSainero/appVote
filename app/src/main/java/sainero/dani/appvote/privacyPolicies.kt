7c8227865d6a609f5ece1b87e669924548a419ec
danielSainero <sainero2002dani.j@gmail.com>
2021-11-01 18:37:18 +0100

    Start creation of layouts skins.

---
+++ app/src/main/java/sainero/dani/appvote/privacyPolicies.kt
@@ -0,0 +1,11 @@
+package sainero.dani.appvote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class privacyPolicies : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_privacy_policies)
+    }
+}
